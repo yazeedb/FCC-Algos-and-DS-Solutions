cd209f8b32917a827e53f5919245b00c6425f340
yazeedb <ybzadough@gmail.com>
2019-09-22 15:03:03 -0400

    add Roman Numeral Converter solution

---
+++ romanNumeralConverter.js
@@ -0,0 +1,32 @@
+function convertToRoman(num) {
+  const lookupTable = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+
+  let accumulator = '';
+
+  for (const key in lookupTable) {
+    const numberValue = lookupTable[key];
+
+    while (numberValue <= num) {
+      num -= numberValue;
+      accumulator += key;
+    }
+  }
+
+  return accumulator;
+}
+
+convertToRoman(36);
